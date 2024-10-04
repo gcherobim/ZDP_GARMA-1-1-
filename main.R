dbe43fd32c390a0fe7ca003ff42a24db9bbc826a
Guilherme <cherobim.guilherme@gmail.com>
2024-10-04 13:32:35 -0300

    tentando alterar o epsilon

--- main.R
+++ main.R
@@ -7,16 +7,26 @@ coefs <- c(-1.8, 0.5, -0.5, -0.3, 4, -0.3)
 tam <- 200
 y <- serie(coefs, tam)
 
-hist(y)
-plot.ts(y)
-table(y)/tam
-acf(y)
+# mut <- cumsum(y) / seq_along(y)
+# 
+# t1 <- tam-1
+# # t2 = n-2
+# Y <- y[3:tam]
+# Mt <- mut[3:tam]
+# yt1 <- y[2:t1]
+# # yt2 = log(yt[1:t2])
+# ymt1 <- log(y[2:t1]) - log(mut[2:t1])
+# # ymt2 = log(yt[1:t2])
+# t <- 3:tam
+# 
+# fit = glm(Mt ~ log(t) + yt1 + ymt1, family = 'poisson')
+# summary(fit)
 
 current_q <- c(0 ,0 ,0 ,0 ,0 ,0)
-iteracoes <- 20000
+iteracoes <- 1000
 chain <- matrix(nrow = (iteracoes + 1), ncol = length(coefs))
 chain[1,] <- current_q
-epsilon <- 0.00000001
+epsilon <- 0.00000000000001
 L <- 15
 c <- 0.1
 s2 <- 200
@@ -24,5 +34,6 @@ medias <- c(0, 0, 0, 0, 0, 0)
 
 for (i in 1:iteracoes-1) {
   chain[i+1,] <- HMC(U, grad_U, epsilon, L, chain[i,])
+  print(i)
 }
 plot(chain[,1])
