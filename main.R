25d7c48ab9dc536b83126c5fa09d706e914000fc
Guilherme <cherobim.guilherme@gmail.com>
2024-10-05 11:10:02 -0300

    procurando o tamanho ideal do passo

--- main.R
+++ main.R
@@ -23,10 +23,10 @@ y <- serie(coefs, tam)
 # summary(fit)
 
 current_q <- c(0 ,0 ,0 ,0 ,0 ,0)
-iteracoes <- 10000
+iteracoes <- 20000
 chain <- matrix(nrow = (iteracoes + 1), ncol = length(coefs))
 chain[1,] <- current_q
-epsilon <- 0.0001
+epsilon <- 0.001
 L <- 20
 c <- 0.1
 s2 <- 200
